b2990349175c61c8b2f539f816c239d950e6c681
hafizh-ender <hafizhrenanto88@gmail.com>
2024-11-29 10:43:09 +0700

    feat(naive_bayes): add Gaussian Naive Bayes algorithm
    Added Gaussian Naive Bayes algorithm, can only receive numerical input

--- src/algorithm/naive_bayes.py
+++ src/algorithm/naive_bayes.py
@@ -1,9 +1,205 @@
+import numpy as np
+import pandas as pd
+
 class GaussianNaiveBayes:
+    """
+    Gaussian Naive Bayes Classifier
+    
+    This classifier predicts the class of an input sample based on the probability of the sample
+    belonging to each class, computed using the Gaussian probability density function.
+    
+    Parameters
+    ----------
+    None
+    
+    Attributes
+    ----------
+    class_priors : dict
+        The prior probabilities of each class.
+    class_means : dict
+        The mean values of each feature for each class.
+    class_variances : dict
+        The variance values of each feature for each class.
+    """
     def __init__(self):
-        pass
-
+        self.class_priors = {}
+        self.class_means = {}
+        self.class_variances = {}
+        self.feature_names = []
+        
     def fit(self, X, y):
-        pass
-
+        """
+        Fit the classifier with the training data. Compute the class priors, means, and variances.
+        
+        Parameters
+        ----------
+        X : list of shape (n_samples, n_features)
+            The training data.
+        y : list of shape (n_samples,)
+            The target labels corresponding to the training data. Can contain strings or other types.
+        """
+        if len(X) != len(y):
+            raise ValueError("X and y must have the same length.")
+        
+         # Save the feature names if X is a DataFrame, else fill with numbers
+        if isinstance(X, pd.DataFrame):
+            self.feature_names = X.columns.tolist()
+        else:
+            self.feature_names = [i for i in range(len(X[0]))]
+        
+        # If X are DataFrame, convert them to a list
+        if isinstance(X, pd.DataFrame):
+            X = X.values.tolist()
+            
+        # Combine the features and labels into a single DataFrame
+        data = pd.DataFrame(X)
+        data['class'] = y
+        
+        # Compute the class priors
+        self.class_priors = data['class'].value_counts(normalize=True).to_dict()
+        
+        # Compute the class means and variances
+        for label in self.class_priors:
+            subset = data[data['class'] == label].drop(columns='class')
+            self.class_means[label] = subset.mean().to_dict()
+            self.class_variances[label] = subset.var().to_dict()
+            
     def predict(self, X):
-        pass
+        """
+        Predict the class labels for the input data.
+        
+        Parameters
+        ----------
+        X : list of shape (n_samples, n_features)
+            The input data to classify.
+        
+        Returns
+        -------
+        y_pred : list of shape (n_samples,)
+            The predicted class labels for each input sample.
+        """        
+        # Check if X is a DataFrame and convert it to a list if it is
+        if isinstance(X, pd.DataFrame):
+            X = X.values.tolist()
+            
+        # Check if X has the same number of features as the training data
+        if len(X[0]) != len(self.class_means[0]):
+            raise ValueError("The number of features in X must be the same as the number of features in the training data.")
+        
+        y_pred = []
+        for x in X:
+            # Compute the probabilities of each class
+            class_probs = {}
+            for label in self.class_priors:
+                class_probs[label] = self._compute_class_probability(x, label)
+                
+            # Determine the class with the highest probability
+            y_pred.append(max(class_probs, key=class_probs.get))
+            
+        return y_pred
+    
+    def _compute_class_probability(self, x, label):
+        """
+        Compute the probability of a sample belonging to a specific class.
+        
+        Parameters
+        ----------
+        x : list or tuple of shape (n_features,)
+            The input sample.
+        label : any
+            The class label for which to compute the probability.
+        
+        Returns
+        -------
+        class_prob : float
+            The computed probability of the sample belonging to the specified class.
+        """
+        class_prob = self.class_priors[label]
+        
+        for i, value in enumerate(x):
+            mean = self.class_means[label][i]
+            variance = self.class_variances[label][i]
+            class_prob *= self._gaussian_pdf(value, mean, variance)
+            
+        return class_prob
+    
+    def _gaussian_pdf(self, x, mean, variance):
+        """
+        Compute the Gaussian probability density function.
+        
+        Parameters
+        ----------
+        x : float
+            The input value.
+        mean : float
+            The mean value of the distribution.
+        variance : float
+            The variance value of the distribution.
+        
+        Returns
+        -------
+        pdf : float
+            The computed probability density function value for the input value.
+        """
+        return (1 / np.sqrt(2 * np.pi * variance)) * np.exp(-((x - mean) ** 2) / (2 * variance))
+    
+    def save(self, filename='naive_bayes.txt'):
+        """
+        Save the classifier model to a text file
+        
+        
+        Parameters
+        ----------
+        filename : str
+            The name of the file to save the model to.
+        """
+        with open(filename, 'w') as file:
+            file.write('class_priors\n')
+            for key, value in self.class_priors.items():
+                file.write(f'{key}: {value}\n')
+                
+            file.write('class_means\n')
+            for key, value in self.class_means.items():
+                file.write(f'{key}: {value}\n')
+                
+            file.write('class_variances\n')
+            for key, value in self.class_variances.items():
+                file.write(f'{key}: {value}\n')
+                
+            file.write('feature_names\n')
+            file.write(f'{self.feature_names}\n')
+            
+            
+    def load(self, filename='naive_bayes.txt'):
+        """
+        Load the classifier model from a text file.
+        
+        Parameters
+        ----------
+        filename : str
+            The name of the file containing the saved model.
+        """
+        with open(filename, 'r') as file:
+            data = file.read()
+
+        lines = data.strip().split('\n')
+        result = {}
+        current_key = None
+
+        for line in lines:
+            if line in ['class_priors', 'class_means', 'class_variances', 'feature_names']:
+                current_key = line
+                result[current_key] = {}
+            elif current_key == 'feature_names':
+                result[current_key] = eval(line)
+            else:
+                key, value = line.split(': ', 1)
+                if current_key in ['class_means', 'class_variances']:
+                    result[current_key][int(key)] = eval(value)
+                else:
+                    result[current_key][int(key)] = float(value)
+
+        self.class_priors = result.get('class_priors', {})
+        self.class_means = result.get('class_means', {})
+        self.class_variances = result.get('class_variances', {})
+        self.feature_names = result.get('feature_names', [])
